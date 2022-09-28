80e73f69af4b89cb5eabc5fa14cf9b6eaf1db73e
Juan Pablo Zapata Atehortua <juanp.zapataa@autonoma.edu.co>
2022-09-27 23:55:36 -0500

    Role permission middleware implemented

--- proyecto/app/Middleware/Permission.ts
+++ proyecto/app/Middleware/Permission.ts
@@ -1,17 +1,48 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import PermissionRole from '../Models/PermissionRole';
-import Permission from '../Models/Permission';
+import PermissionRole from '../Models/PermissionsRole'
+import Permission from '../Models/Permission'
+import User from 'App/Models/User'
+import Role from 'App/Models/Role'
+import { AuthenticationException } from '@adonisjs/auth/build/standalone'
 
 export default class Permission {
-  public async handle({auth, request, response}: HttpContextContract, next: () => Promise<void>) {
-    console.log(request);
-    let url = request.url();
-    const method = request.method();
-    const parts = url.split("/");
+  public async handle({ auth, request, response }: HttpContextContract, next: () => Promise<void>) {
+    const url = request.url()
+    const method = request.method()
+    const parts = url.split('/')
 
-    // TODO: verificar que el rol tenga determinados permisos
+    let new_url = "/"+parts[1]
+
+    const user_id = auth.user.id
+    const user_role_id = auth.user.role_id
+
+    let has_permissions:boolean 
+    has_permissions = await this.verifyPermissions(user_role_id, new_url, method)
+
+    if (!has_permissions) {
+      /**
+       * The user doesnt have the right permissions for its request
+       */
+       throw new AuthenticationException(
+        'Missing right permissions',
+        'E_MISSINGS_PERMISSIONS',
+        this.redirectTo
+      )
+    } 
+
+    // The user has permission
+    console.log("Permiso otorgado")
     await next()
+  
   }
 
-  private 
+  protected async verifyPermissions(user_role_id, url, method):boolean{
+    let role_permissions = await PermissionRole.query().where('role_id', '=', user_role_id)
+    for(let i=0;i<role_permissions.length;i++){
+      let permission = await Permission.find(role_permissions[i].permission_id)
+      if (permission.url == url && permission.method == method.toUpperCase()) 
+        return true
+    }
+    return false
+  }
 }
