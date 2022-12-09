edcd32b9f7f4eaa0f03e65cbb6cffe8935e5120c
Juan Pablo Zapata Atehortua <juanp.zapataa@autonoma.edu.co>
2022-12-09 16:36:06 -0500

    a

--- proyecto/start/routes/parkings.ts
+++ proyecto/start/routes/parkings.ts
@@ -2,14 +2,15 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(()=>{
     //PARKING
-    Route.get("/parking","ParkingsController.index");
+    
     Route.post("/parking","ParkingsController.store");
-    Route.get("/parking/:id","ParkingsController.show");
+    
     Route.put("/parking/:id","ParkingsController.update");
     Route.delete("/parking/:id","ParkingsController.destroy");
 
 }).middleware(['auth:api','permission'])
-
+Route.get("/parking","ParkingsController.index");
+Route.get("/parking/:id","ParkingsController.show");
 Route.post("/parkingRating","ParkingRatingsController.store");
 Route.get("/parkingRating","ParkingRatingsController.index");  
 Route.get("/parkingRating/:id","ParkingRatingsController.show");
@@ -20,12 +21,12 @@ Route.group(()=>{
     Route.put("/parkingRating/:id","ParkingRatingsController.update");
     Route.delete("/parkingRating/:id","ParkingRatingsController.destroy");
 }).middleware(['auth:api','permission'])
-
+Route.get("/parkingSpot","ParkingSpotsController.index");
 Route.put("/parkingSpot/:id","ParkingSpotsController.update");
 Route.get("/parkingSpot/parking/:id_parking","ParkingSpotsController.indexParking");
 Route.group(()=>{
     //PARKINGSPOT
-    Route.get("/parkingSpot","ParkingSpotsController.index");
+    
     
     Route.post("/parkingSpot","ParkingSpotsController.store");
     Route.get("/parkingSpot/:id","ParkingSpotsController.show");
